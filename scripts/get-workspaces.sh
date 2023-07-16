9a7d6ab4f009ead4e6a929ecdc5d7c33fb09f3b2
Skane-Gahi <skanguy17@orange.fr>
2023-07-16 11:48:53 +0200

    refactor: Workspace widget customisation and styling

--- scripts/get-workspaces.sh
+++ scripts/get-workspaces.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+set -e
 
-spaces () {
-	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-	seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
+# Get workspace number from args
+WORKSPACE_NUMBER=$1
+
+workspaces () {
+    # Retrieve active workspaces in json format
+	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | 
+        jq 'map({key: .id | tostring, value: .windows}) | from_entries')
+	
+    # Create workspaces mapping windows with the previous result
+    seq 1 $WORKSPACE_NUMBER | 
+        jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc \
+            'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
 }
 
-spaces
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-	spaces
+# Call the function once
+workspaces
+# Then, call it each time the workspace changes
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | 
+    while read -r line; do workspaces
 done
