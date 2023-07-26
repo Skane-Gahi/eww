2281cdf5f01326cfad3472299840a21613ec139d
Skane-Gahi <skanguy17@orange.fr>
2023-07-26 19:00:13 +0200

    feat: real time bluetooth device retrieval script

---
+++ scripts/bluetooth/get_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+coproc scan { bluetoothctl ; }
+echo -e "scan on\n" >&"${scan[1]}"
+
+while true; do
+    output=$(bluetoothctl devices)
+    echo "$output" | 
+        jq -sR '[split("\n") | .[] | split("Device ")[1:] | .[] | {addr: .[:17], name: .[18:]}]'
+    sleep 1
+done
