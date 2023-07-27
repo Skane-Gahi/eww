e0101ffce34da6324e4d797c3b130e8474736d6e
Skane-Gahi <skanguy17@orange.fr>
2023-07-27 15:23:18 +0200

    style: jq strings multiline for readability

--- scripts/bluetooth/get_all.sh
+++ scripts/bluetooth/get_all.sh
@@ -7,6 +7,15 @@ echo -e "scan on\n" >&"${scan[1]}"
 while true; do
     output=$(bluetoothctl devices)
     echo "$output" | 
-        jq -sR '[split("\n") | .[] | split("Device ")[1:] | .[] | {addr: .[:17], name: .[18:]}]'
+        jq -sR '[
+            split("\n") | 
+            .[] | 
+            split("Device ")[1:] |
+            .[] |
+            {
+                addr: .[:17], 
+                name: .[18:]
+            }
+        ]'
     sleep 1
 done
--- scripts/bluetooth/get_paired.sh
+++ scripts/bluetooth/get_paired.sh
@@ -3,7 +3,13 @@ set -e
 
 devices=$(
     bluetoothctl devices Paired | 
-        jq -sR '[split("\n") | .[:-1] | .[] | split(" ")[2:] | join(" ")]'
+        jq -sR '[
+            split("\n") |
+            .[:-1] |
+            .[] |
+            split(" ")[2:] |
+            join(" ")
+        ]'
 )
 
 echo $devices
